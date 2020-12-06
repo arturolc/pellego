c4b2fd07da030ceb63ab9f080d7c108b194411b9
hebd1 <elihebdon@gmail.com>
2020-12-05 19:46:20 -0700

    Add import button

--- app/src/main/java/com/example/pellego/ui/library/LibraryFragment.java
+++ app/src/main/java/com/example/pellego/ui/library/LibraryFragment.java
@@ -1,6 +1,8 @@
 package com.example.pellego.ui.library;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,16 +14,20 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.pellego.FileDialog;
 import com.example.pellego.R;
 
+import java.io.File;
+
 /**********************************************
-    Chris Bordoy
+    Chris Bordoy & Eli Hebdon
 
     The Library Component
  **********************************************/
 public class LibraryFragment extends Fragment {
 
     private LibraryViewModel libraryViewModel;
+    private FileDialog fileDialog;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +41,26 @@ public class LibraryFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        // handle import button click
+        View button_import = root.findViewById(R.id.button_import);
+        button_import.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File mPath = new File(Environment.getExternalStorageDirectory() + "//DIR//");
+                fileDialog = new FileDialog(getActivity(), mPath);
+                fileDialog.addFileListener(new FileDialog.FileSelectedListener() {
+                    public void fileSelected(File file) {
+                        Log.d(getClass().getName(), "selected file " + file.toString());
+                    }
+                });
+
+                fileDialog.showDialog();
+
+            }
+        });
         return root;
+
+
     }
 }
