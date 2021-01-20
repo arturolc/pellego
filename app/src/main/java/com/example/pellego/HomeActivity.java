1a076ac2ccb7c2f4f9393cde9b378da18f473f19
hebd1 <elihebdon@gmail.com>
2021-01-19 19:00:00 -0700

    Create navigation for hamburger menu and back button

--- app/src/main/java/com/example/pellego/HomeActivity.java
+++ app/src/main/java/com/example/pellego/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -43,59 +44,37 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_Pellego);
         setContentView(R.layout.activity_home);
-        drawer = findViewById(R.id.home_layout);
-        bottomNavigationView = findViewById(R.id.bottom_nav_view);
-        toolbar = findViewById(R.id.toolbar);
 
+        // Set toolbar as action bar
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
-        // setup bottom nav menu
+        // setup bottom nav and drawer nav menus
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        drawer = findViewById(R.id.home_layout);
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-
         appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_settings, R.id.nav_library, R.id.nav_learn, R.id.nav_progress, R.id.nav_profile, R.id.nav_terms_and_conditions)
+                .setDrawerLayout(drawer)
                 .build();
         NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
-
-        // Setup drawer nav menu navigation
+        // Attach nav drawer to nav controller
         NavigationView drawerNavigationView = findViewById(R.id.side_nav_view);
         NavigationUI.setupWithNavController(drawerNavigationView, navController);
-        NavigationUI.setupActionBarWithNavController(this, navController);
-        NavigationUI.setupActionBarWithNavController(this, navController, drawer);
-
-        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState(); // rotates hamburger icon
-
-
-
-        // toolbar at top of screen that contains hamburger drawer menu button
-//
-//        ActionBar actionbar = getSupportActionBar();
-//        actionbar.setHomeAsUpIndicator(R.color.white);
-//        actionbar.setDisplayHomeAsUpEnabled(true);
-//        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
-
-
 
     }
 
     /**
-     * Don't know what this does ..
+     * Called when the user presses the back button
      * @return
      */
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
 
-
-    });
 
 }
