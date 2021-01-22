68119ce7b6a48dca3358eecc30125e4a37bccc5b
arturolc <arturo.lara@utah.edu>
2021-01-21 20:09:27 -0700

    confirmation code

--- app/src/main/java/com/example/pellego/LoginActivity.java
+++ app/src/main/java/com/example/pellego/LoginActivity.java
@@ -2,7 +2,14 @@ package com.example.pellego;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.amplifyframework.core.Amplify;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +18,28 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void signIn(View view) {
+        EditText email = findViewById(R.id.editTextTextEmailAddress);
+        EditText password = findViewById(R.id.editTextTextPassword);
+
+        Amplify.Auth.signIn(email.getText().toString(),
+                            password.getText().toString(),
+                            success -> {
+                                Log.i("AUTHENTICATION", success.toString());
+                                Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(i);
+                                finish();
+                            },
+                            error -> Log.e("AUTHENTICATION", error.toString()));
+    }
+
+    public void moveToRegister(View view) {
+        Intent i = new Intent(LoginActivity.this,
+                RegisterActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+
 }
--- app/src/main/java/com/example/pellego/RegisterActivity.java
+++ app/src/main/java/com/example/pellego/RegisterActivity.java
@@ -2,13 +2,67 @@ package com.example.pellego;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.amplifyframework.auth.AuthUserAttributeKey;
+import com.amplifyframework.auth.options.AuthSignUpOptions;
+import com.amplifyframework.core.Amplify;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+
+    public void register(View view) {
+        firstName = ((EditText)findViewById(R.id.firstNameText)).getText().toString();
+        lastName = ((EditText)findViewById(R.id.lastNameText)).getText().toString();
+        email = ((EditText)findViewById(R.id.emailText)).getText().toString();
+        EditText password = findViewById(R.id.passwordText);
+
+        Amplify.Auth.signUp(
+                email,
+                password.getText().toString(),
+                AuthSignUpOptions.builder().userAttribute(AuthUserAttributeKey.email(), email).build(),
+                result -> Log.i("AUTHENTICATION", "Result: " + result.toString()),
+                error -> Log.e("AUTHENTICATION", "Sign up failed", error)
+        );
+
+        findViewById(R.id.layout1).setVisibility(8);
+        findViewById(R.id.layout2).setVisibility(0);
+    }
+
+    public void confirm(View view) {
+        EditText confirmationCode = findViewById(R.id.confirmCodeText);
+
+        Amplify.Auth.confirmSignUp(
+                email,
+                confirmationCode.getText().toString(),
+                result ->
+                {
+                    Intent i = new Intent(RegisterActivity.this,
+                            LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                    Log.i("AUTHENTICATION", result.toString());
+                },
+                error -> Log.e("AUTHENTICATION", error.toString())
+        );
+
+    }
+
+
+
+
 }
