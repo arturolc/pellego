03098a3bfaa8b872091c39476a35f8f67a47b908
u1215936 <u1215936@utah.edu>
2021-02-04 11:47:09 -0700

    Cleaned up the code. Navigation Component is in place. One residual bug left with backtracking causing double bottom nav menus to display

--- app/src/main/java/com/example/pellego/HomeActivity.java
+++ app/src/main/java/com/example/pellego/HomeActivity.java
@@ -1,23 +1,15 @@
 package com.example.pellego;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 
-//import com.example.pellego.ui.rsvp.RsvpIntroFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
@@ -29,51 +21,38 @@ import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private DrawerLayout drawer;
     private NavController navController;
     private ActionBarDrawerToggle toggle;
     BottomNavigationView bottomNavigationView;
     private Toolbar toolbar;
 
-    private String user_name;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_Pellego);
         setContentView(R.layout.activity_home);
-//
-//        // Set toolbar as action bar
+
+        // Set toolbar as action bar
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//
-//        // setup bottom nav and drawer nav menus
-        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        // setup bottom nav and side nav menus
         drawer = findViewById(R.id.home_activity_layout);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_settings, R.id.nav_library, R.id.nav_learn, R.id.nav_progress,
-                R.id.nav_profile, R.id.nav_terms_and_conditions, R.id.nav_sign_out)
-                .build();
-        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
         navController.navigate(R.id.nav_library);
 
-         //Attach nav drawer to nav controller
+        //Attach side nav drawer to nav controller
         NavigationView drawerNavigationView = findViewById(R.id.side_nav_view);
         NavigationUI.setupWithNavController(drawerNavigationView, navController);
     }
-
-    /**
-     * Called when the user presses the back button
-     * @return
-     */
-    @Override
-    public boolean onSupportNavigateUp() {
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-
-
 }
