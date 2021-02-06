12fe7dd687a80081c6e2b84e84a3179758286f6f
arturolc <arturo.lara@utah.edu>
2021-02-05 17:31:17 -0700

    added create table queries to database helper

--- app/src/main/java/com/example/pellego/DatabaseHelper.java
+++ app/src/main/java/com/example/pellego/DatabaseHelper.java
@@ -14,13 +14,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTableQuery = "";
+        String createUsersQuery =   "CREATE TABLE IF NOT EXISTS Users (" +
+                                    "UID INTEGER UNIQUE NOT NULL AUTOINCREMENT," +
+                                    "UniqueIdentifier VARCHAR(320) UNIQUE NOT NULL," +
+                                    "FirstName VARCHAR(50) NOT NULL," +
+                                    "LastName VARCHAR(50) NOT NULL," +
+                                    "PRIMARY KEY (UID))";
 
-        db.execSQL(createTableQuery);
+        String createLibraryQuery = "CREATE TABLE IF NOT EXISTS Library (" +
+                                    "UID INT NOT NULL," +
+                                    "BID INT NOT NULL," +
+                                    "PRIMARY KEY(UID, BID))";
+
+        String createBooksQuery =   "CREATE TABLE IF NOT EXISTS Books (" +
+                                    "BID INT UNIQUE NOT NULL AUTOINCREMENT," +
+                                    "BookName VARCHAR(200) NOT NULL," +
+                                    "Author VARCHAR(100) NOT NULL," +
+                                    "FilePath VARCHAR(255) NOT NULL," +
+                                    "PRIMARY KEY (BID))";
+
+        db.execSQL(createUsersQuery);
+        db.execSQL(createLibraryQuery);
+        db.execSQL(createBooksQuery);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+
 }
