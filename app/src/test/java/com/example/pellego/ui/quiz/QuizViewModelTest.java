cd12486e1800704dd5bdd0c15c2d490da4d2a93d
hebd1 <elihebdon@gmail.com>
2021-02-13 17:38:55 -0700

    Add simple unit test

--- app/src/test/java/com/example/pellego/ui/quiz/QuizViewModelTest.java
+++ app/src/test/java/com/example/pellego/ui/quiz/QuizViewModelTest.java
@@ -15,9 +15,9 @@ public class QuizViewModelTest extends TestCase {
     public void setUp() throws Exception {
         super.setUp();
     }
-
-    @Mock
-    QuizViewModel quizViewModel = new QuizViewModel();
+//
+//    @Mock
+//    QuizViewModel quizViewModel = new QuizViewModel();
 
 // not working as we cannot test live data not on the android thread
     @Test
