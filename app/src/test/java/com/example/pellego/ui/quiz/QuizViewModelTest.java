751f1cbe5c5bc395917bd65e8f60298878111251
Eli Hebdon <elihebdon@gmail.com>
2021-02-16 21:38:19 -0700

    Unit tests, CI/CD, and README

--- app/src/test/java/com/example/pellego/ui/quiz/QuizViewModelTest.java
+++ app/src/test/java/com/example/pellego/ui/quiz/QuizViewModelTest.java
@@ -1,31 +1,55 @@
 package com.example.pellego.ui.quiz;
+import androidx.test.core.app.ApplicationProvider;
+
+import android.content.Context;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
+@RunWith(AndroidJUnit4.class)
 public class QuizViewModelTest extends TestCase {
 
-//    @Rule
-//    public InstantTaskExecutorRule instantExecutorRule = new InstantTaskExecutorRule();
+    @Rule
+    public InstantTaskExecutorRule instantExecutorRule = new InstantTaskExecutorRule();
+    private Context context = ApplicationProvider.getApplicationContext();
 
-    public void setUp() throws Exception {
-        super.setUp();
-    }
 
     @Mock
     QuizViewModel quizViewModel = new QuizViewModel();
 
-// not working as we cannot test live data not on the android thread
-    @Test
-    public void testPopulateQuestionBank() {
-//        QuizViewModel quizViewModel = new QuizViewModel();
-//            quizViewModel.setDifficulty("intermediate");
-//            quizViewModel.populateQuestionBank();
-//            assertNotNull(quizViewModel.getNextQuestion());
-        assertTrue(true);
+    @Before
+    public void initialize() {
+        quizViewModel.setDifficulty("intermediate");
+        quizViewModel.setWPM(120);
+        quizViewModel.setModule("metaguiding");
     }
+
+    @Test
+    public void testInitialization() {
+
+        assertEquals("intermediate", quizViewModel.getDifficulty());
+        assertTrue(120 == quizViewModel.getWPM());
+        assertEquals("metaguiding", quizViewModel.getModule());
+    }
+
+
+    @Test
+    public void testQuestions() {
+        quizViewModel.populateQuestionBank();
+        String q = quizViewModel.getNextQuestion();
+        assertNotNull(q);
+        assertFalse(quizViewModel.isLastQuestion());
+        assertNotNull(quizViewModel.getNextAnswers());
+        assertTrue(quizViewModel.getCorrectIndex() > -1 && quizViewModel.getCorrectIndex() < 4);
+        quizViewModel.incrementQuestionCount();
+        assertNotSame(quizViewModel.getNextQuestion(), q);
+ }
 }
