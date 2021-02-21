328969eb03762ad75fecf0de61a786c61ed53925
hebd1 <elihebdon@gmail.com>
2021-02-21 15:09:13 -0700

    Remove uncessary methods

--- app/src/main/java/com/gitlab/capstone/pellego/fragments/library/LibraryFragment.java
+++ app/src/main/java/com/gitlab/capstone/pellego/fragments/library/LibraryFragment.java
@@ -681,37 +681,6 @@ public class LibraryFragment extends Fragment implements MainActivity.SearchList
         super.onDestroy();
         books.clearTasks();
     }
-    protected void setMenuBackground() {
-        // Log.d(TAG, "Enterting setMenuBackGround");
-        getLayoutInflater().setFactory(new LayoutInflater.Factory() {
-            public View onCreateView(String name, Context context, AttributeSet attrs) {
-                if (name.equalsIgnoreCase("com.android.internal.view.menu.IconMenuItemView")) {
-                    try { // Ask our inflater to create the view
-                        LayoutInflater f = getLayoutInflater();
-                        final View view = f.createView(name, null, attrs);
-                        /* The background gets refreshed each time a new item is added the options menu.
-                         * So each time Android applies the default background we need to set our own
-                         * background. This is done using a thread giving the background change as runnable
-                         * object */
-                        new Handler().post(new Runnable() {
-                            public void run() {
-                                // sets the background color
-                                view.setBackgroundResource(R.color.light_blue);
-                                // sets the text color
-                                ((TextView) view).setTextColor(Color.BLACK);
-                                // sets the text size
-                                ((TextView) view).setTextSize(18);
-                            }
-                        });
-                        return view;
-                    } catch (InflateException e) {
-                    } catch (ClassNotFoundException e) {
-                    }
-                }
-                return null;
-            }
-        });
-    }
 
         @Override
     public void onCreateOptionsMenu(final Menu menu, MenuInflater inflater) {
