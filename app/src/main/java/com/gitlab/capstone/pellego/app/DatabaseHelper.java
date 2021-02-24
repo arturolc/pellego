b9e8fc34ed3f61ade3ee6a85901a63b7d210e104
Joanna Lowry <readingnature467@outlook.com>
2021-02-23 20:57:40 -0700

    The app now contacts the database via the DatabaseHelper to add a user to the database.

--- app/src/main/java/com/gitlab/capstone/pellego/app/DatabaseHelper.java
+++ app/src/main/java/com/gitlab/capstone/pellego/app/DatabaseHelper.java
@@ -4,18 +4,32 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**********************************************
- Arturo Lara
+ Arturo Lara and Joanna Lowry
  DatabaseHelper uses SQLite local database to cache online data
  so application can work offline
  **********************************************/
 public class DatabaseHelper extends SQLiteOpenHelper {
+    private Context ctx;
 
     public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, "pellego.db", null, 1);
+        ctx = context;
     }
 
     @Override
@@ -63,9 +77,62 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         long res = db.insert("Users", null, cv);
 
+        //talk to the cloud database here
+        //what happens if we are unable to establish a connection to the aws server?
+        //we could queue the users and add them whenever we have to talk to the database again
+        //or we could rollback adding the user...TODO
+        getApiData(user.getFirstName(), user.getLastName(), user.getEmail(), new DatabaseHelper.ResponseCallback() {
+            @Override
+            public void onResponse(Object response) {
+                //currently does nothing
+            }
+        });
+
+
         return res != -1;
     }
 
+    public interface ResponseCallback
+    {
+        void onResponse(Object response);
+    }
+
+    private void getApiData(String first, String last, String email, DatabaseHelper.ResponseCallback responseCallBack)
+    {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(ctx);
+        String url ="http://54.176.198.201:5000/user/add";
+        JSONObject user = new JSONObject();
+        try {
+            user.put("email", email);
+            user.put("name", first + " " + last);
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+        JSONArray request = new JSONArray();
+        request.put(user);
+
+        // Request a json response from the provided URL.
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.POST, url, request, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //Currently does nothing but we could consider doing something in the future
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("error", error.toString());
+                        //Currently does nothing. If it fails we should rollback changes to the local database and not let the user log in
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonArrayRequest);
+    }
     public boolean addBook(BookModel book) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
