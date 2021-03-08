08f0cb4184460a39efbb861885a6f77d40fa71ca
hebd1 <elihebdon@gmail.com>
2021-03-07 23:05:32 -0700

    Remove some uneccessary commented out code

--- app/src/main/java/com/gitlab/capstone/pellego/activities/SplashActivity.java
+++ app/src/main/java/com/gitlab/capstone/pellego/activities/SplashActivity.java
@@ -88,9 +88,6 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // Errors with amplify
-//                if (i == null) i = new Intent(SplashActivity.this,
-//                        MainActivity.class);
                 startActivity(i);
                 finish();
             }
